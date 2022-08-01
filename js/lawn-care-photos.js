1ce7f10577939d8c5f6a67f81ed44284699a65c0
Aleksandr <alex.avramenko2004@gmail.com>
2022-08-01 11:37:28 -0400

    08/01/2022 - patch #17

--- js/lawn-care-photos.js
+++ js/lawn-care-photos.js
@@ -1,10 +1,50 @@
-let list = document.querySelectorAll(".lawn-care-item");
-let listArray = Array.from(list);
-
-let photo = document.getElementById("lawn-care-photo")
+let list = document.querySelectorAll(".lawn-care-item"); // Forming a NodeList of all items with class "lawn-care-item"
 
 
-for(i = 0; i < listArray.length; i++)
-{
-    
-}   
+list[0].addEventListener("click", function photoChange(){
+    document.getElementById("lawn-care-photo").src="img/lawn-care/0.svg"
+});
+
+list[1].addEventListener("click", function photoChange(){
+    document.getElementById("lawn-care-photo").src="img/lawn-care/1.svg"
+});
+
+list[2].addEventListener("click", function(){
+    document.getElementById("lawn-care-photo").src="img/lawn-care/2.svg"
+});
+
+list[3].addEventListener("click", function(){
+    document.getElementById("lawn-care-photo").src="img/lawn-care/3.svg"
+});
+
+list[4].addEventListener("click", function(){
+    document.getElementById("lawn-care-photo").src="img/lawn-care/4.svg"
+});
+
+list[5].addEventListener("click", function(){
+    document.getElementById("lawn-care-photo").src="img/lawn-care/5.svg"
+});
+
+list[6].addEventListener("click", function(){
+    document.getElementById("lawn-care-photo").src="img/lawn-care/6.svg"
+});
+
+list[7].addEventListener("click", function(){
+    document.getElementById("lawn-care-photo").src="img/lawn-care/7.svg"
+});
+
+list[8].addEventListener("click", function(){
+    document.getElementById("lawn-care-photo").src="img/lawn-care/8.svg"
+});
+
+list[9].addEventListener("click", function(){
+    document.getElementById("lawn-care-photo").src="img/lawn-care/9.svg"
+});
+
+list[10].addEventListener("click", function(){
+    document.getElementById("lawn-care-photo").src="img/lawn-care/10.svg"
+});
+
+list[11].addEventListener("click", function(){
+    document.getElementById("lawn-care-photo").src="img/lawn-care/11.svg"
+});
