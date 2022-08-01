73fb331e322cefdd06f6a155b0d8361dfb8e63c6
Aleksandr <alex.avramenko2004@gmail.com>
2022-07-31 23:14:17 -0400

    07/31/2022 - patch #16

---
+++ js/lawn-care-photos.js
@@ -0,0 +1,10 @@
+let list = document.querySelectorAll(".lawn-care-item");
+let listArray = Array.from(list);
+
+let photo = document.getElementById("lawn-care-photo")
+
+
+for(i = 0; i < listArray.length; i++)
+{
+    
+}   
