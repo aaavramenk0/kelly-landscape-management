54983b06949346f5876291c3efb29ba2250076db
Aleksandr <alex.avramenko2004@gmail.com>
2022-08-02 16:25:36 -0400

    08/02/2022 - patch #19

---
+++ js/landscaping-photos.js
@@ -0,0 +1,50 @@
+let list = document.querySelectorAll(".landscaping-item"); // Forming a NodeList of all items with class "landscaping-item"
+
+
+list[0].addEventListener("click", function(){
+    document.getElementById("landscaping-photo").src="img/landscaping/0.svg"
+});
+
+list[1].addEventListener("click", function(){
+    document.getElementById("landscaping-photo").src="img/landscaping/1.svg"
+});
+
+list[2].addEventListener("click", function(){
+    document.getElementById("landscaping-photo").src="img/landscaping/2.svg"
+});
+
+list[3].addEventListener("click", function(){
+    document.getElementById("landscaping-photo").src="img/landscaping/3.svg"
+});
+
+list[4].addEventListener("click", function(){
+    document.getElementById("landscaping-photo").src="img/landscaping/4.svg"
+});
+
+list[5].addEventListener("click", function(){
+    document.getElementById("landscaping-photo").src="img/landscaping/5.svg"
+});
+
+list[6].addEventListener("click", function(){
+    document.getElementById("landscaping-photo").src="img/landscaping/6.svg"
+});
+
+list[7].addEventListener("click", function(){
+    document.getElementById("landscaping-photo").src="img/landscaping/7.svg"
+});
+
+list[8].addEventListener("click", function(){
+    document.getElementById("landscaping-photo").src="img/landscaping/8.svg"
+});
+
+list[9].addEventListener("click", function(){
+    document.getElementById("landscaping-photo").src="img/landscaping/9.svg"
+});
+
+list[10].addEventListener("click", function(){
+    document.getElementById("landscaping-photo").src="img/landscaping/10.svg"
+});
+
+list[11].addEventListener("click", function(){
+    document.getElementById("landscaping-photo").src="img/landscaping/11.svg"
+});
